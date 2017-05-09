3f1ef8adf9184aa3d0246d404aa79acfd0c01dd8
GGH <ggh@d207-023-191-034.wireless.sfu.ca>
2017-05-09 13:26:28 -0700

    - coded r(., .) p6 (tested as much as possible at this point) - cleaned up the file - moving on to R matrix function

--- msc/R/covariance.R
+++ msc/R/covariance.R
@@ -1,36 +1,72 @@
-# R matrix for a vector of inputs
-Rmat <- function(x1, x2, sig2 = 1, l = 1) {
+################################################################################
+# Covariance of yi, yj with inputs xi, xj respectively #########################
+################################################################################
+r <- function(xi, xj,
+              sig2,
+              l = NULL, ltrans = NULL, lambda = 5/2) {
 
-  # r(., .) function (p6)
-  r <- function(xi, xj, sig2 = sig2, ltrans = sig2) {
+  # xi, xj: d dimensional vectors
+  # sig2: constant variance parameter; non-negative real number
+  # l: d dimensional length scale parameters; non-negative
+  # ltrans: \sqrt(2 * \lambda) / l; d-dimensional non-negative vector
+  # lambda: non-negative parameter
+  # RETURN: r(xi, xj) defined on p6
 
-    # g(.): from Matern classs of covariance functions
-    ## only need g = g_k because dim(x) = 1
-    ## modified bessel function of the second kind
-    ## https://stat.ethz.ch/pipermail/r-help/2002-December/027850.html
-    g <- function(xi, xj, ltrans) {
+  # ERROR HANDLING #############################################################
+  if (missing(xi) || missing(xj)) {stop("Arguments xi, xj are missing.")}
+  if (length(xi) != length(xj)) {stop("Vectors xi, xj must be same length.")}
 
-      # 2^(1 - lambda) / gamma(lambda) *
-      #   ( sqrt(2 * lambda) * abs(xi - xj) / l ) ^ lambda *
-      #   besselK(sqrt(2 * lambda) * abs(xi - xj) / l, nu = lambda)
+  if (missing(sig2)) {stop("Constant variance parameter sig2 missing.")}
 
-      ## version where ltrans = sqrt(2 * lambda) / l
+  if (is.null(l) & is.null(ltrans)) {
+    stop("Length scale parameter l or ltrans missing.")
+  }
+  if (!is.null(l) & !is.null(ltrans)) {
+    stop("Specify length scale parameter as either l or ltrans, not both.")
+  }
 
-      # FROM NOW ON l = ltrans
-      lambda <- 5/2
-      2^(1 - lambda) / gamma(lambda) *
-        ( ltrans * abs(xi - xj) ) ^ lambda *
-        besselK( ltrans * abs(xi - xj), nu = lambda)
+  # always work with ltrans ####################################################
+  if (is.null(ltrans)) {ltrans <- sqrt(2 * lambda) / l}
+  if (length(xi) != length(ltrans)) {
+    stop("ltrans must be same length as xi, xj.")
+  }
 
-    }
+  # if xi == xj ################################################################
+  if (isTRUE(all.equal(xi, xj))) {return(sig2)}
 
-    if (xi == xj) {
-      return(sig2)
-    } else {
-      return(sig2 * g(xi, xj, ltrans))
-    }
+  # g FUNCTION START ###########################################################
+  # g(.): from Matern classs of covariance functions
+  ## modified bessel function of the second kind
+  ## https://stat.ethz.ch/pipermail/r-help/2002-December/027850.html
+  g <- function(input) {
+    # input: 3-dimensional vector c(xik, xjk, ltransk)
+    ## xik, xjk: kth entry of vectors xi, xj respectively
+    ## ltransk: kth entry of ltrans as defined for r(., .); always ltrans, not l
+
+    xik <- input[1]
+    xjk <- input[2]
+    ltransk <- input[3]
+
+    2^(1 - lambda) / gamma(lambda) *
+      ( ltrans * abs(xik - xjk) ) ^ lambda *
+      besselK( ltrans * abs(xi - xj), nu = lambda)
 
   }
+  # g FUNTION END ##############################################################
+
+  # xi != xj ###################################################################
+  xixjltrans <- cbind(xi, xj, ltrans)
+  covxixj <- sig2 * prod( apply(xixjltrans, MARGIN = 1, FUN = g) )
+
+  return(covxixj)
+
+}
+
+################################################################################
+# Covariance matrix for a vector of inputs #####################################
+################################################################################
+
+Rmat <- function(x1, x2, sig2 = 1, l = 1) {
 
   R <- matrix(NA, nrow = length(x1), ncol = length(x2))
 
