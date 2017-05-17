73d997838a2575a83325ffd580337a6e279116ba
grace <561GH@users.noreply.github.com>
2017-05-17 15:58:46 -0700

    making sure to actually name the output rds file

--- overnight.R
+++ overnight.R
@@ -4,4 +4,5 @@ install.packages(c("foreach", "mvtnorm"))
 install.packages(file.choose(), repos = NULL, type = "source", dependencies = TRUE)
 
 
-saveRDS(chains, file = "C:\Users\gghsu\Desktop")
+saveRDS(chains, file = "C:\Users\gghsu\Desktop\eta0chains.rds")
+saveRDS(out, file = "C:\Users\gghsu\Desktop\eta0out.rds")
