31a603d20b2e5a8fe7241a67ac8bfffe3d106775
GGH <ggh@d207-023-184-141.wireless.sfu.ca>
2017-05-17 12:20:25 -0700

    - fixing the covariance matrix for (ystar, yprime) distribution (\Sigma) - still currently weird/probably wrong because derivatives of matern have that sign term and so I’m forcing it to be symmetric right now but it’s not passing the test that it should

--- eta0.R
+++ eta0.R
@@ -9,25 +9,35 @@ log.posterior <- function(l, sig2,
   # given = list(y = y, xstar = xstar, xprime = xprime, x = x)
   # xstar, xprime, x: careful likely have to cbind() themn to make them columns
 
-  ## unpacking given
+  ## unpacking given (cbind to make sure they are matrices)
   y <- given$y
-  xstar <- given$xstar
-  xprime <- given$xprime
-  x <- given$x
+  xstar <- cbind(given$xstar)
+  xprime <- cbind(given$xprime)
+  x <- cbind(given$x)
 
   ## means and covariances
   m <- c(rep(mean(ystar), length(ystar)),
          rep(mean(yprime), length(yprime)))
   my <- rep(mean(y), length(y))
 
-  S <- covMatrix(X = rbind(xstar, xprime), sig2 = sig2,
-                 covar.fun = r.matern, l = l)
-  Syy <- covMatrix(X = x, sig2 = sig2,
-                   covar.fun = r.matern, l = l)
+  S11 <- covMatrix(X = xstar, sig2 = sig2, covar.fun = r.matern, l = l)
+  S22 <- covMatrix(X = xprime, sig2 = sig2, covar.fun = r.matern2, l = l)
+  S12 <- covMatrix(X = xstar, X2 = xprime,
+                   sig2 = sig2, covar.fun = r.matern1, l = l)
+  S <- rbind(cbind(S11, S12),
+             cbind(t(S12), S22))
+  # S21 <- covMatrix(X = xprime, X2 = xstar,
+  #                  sig2 = sig2, covar.fun = r.matern1, l = l)
+  # all.equal(S12, t(S21))  # should be TRUE...
+
+  Syy <- covMatrix(X = x, sig2 = sig2, covar.fun = r.matern, l = l)
 
   ## use log scale because numbers are so tiny
   log.d.ystaryprime <- dmvnorm(c(ystar, yprime), mean = m, sigma = S, log = TRUE)
   log.d.y <- dmvnorm(y, mean = my, sigma = Syy, log = TRUE)
+
+  cat("log.d.ystaryprime:", log.d.ystaryprime)
+  cat("log.d.y:", log.d.y)
 
   return( log( dchisq(sqrt(5) / l, df = 1) ) + log( dchisq(sig2, df = 5) ) +
             log.d.ystaryprime + log.d.y )
