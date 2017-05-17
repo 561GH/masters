d32bb5bd175ff7d2fc3c66cf4bbe382b3defc0ff
grace <561GH@users.noreply.github.com>
2017-05-17 15:54:36 -0700

    added some more to overnight code

--- overnight.R
+++ overnight.R
@@ -1,3 +1,7 @@
 # Running code overnight
 
+install.packages(c("foreach", "mvtnorm"))
 install.packages(file.choose(), repos = NULL, type = "source", dependencies = TRUE)
+
+
+saveRDS(chains, file = "C:\Users\gghsu\Desktop")
