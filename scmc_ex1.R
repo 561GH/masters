7706afa2c1cc4ec0cc4364ac2baecf17459d7541
GGH <ggh@d207-023-185-117.wireless.sfu.ca>
2017-06-26 18:47:08 -0700

    - majorly rewriting alg1 SCMC for example 1 REALLY REALLY NEED TO GET THE INDICES FOR t SORTED OUT

--- scmc_ex1.R
+++ scmc_ex1.R
@@ -1,28 +1,42 @@
 # SCMC toy example
 
 ## setup
-ytrue <- function(x) {log(20*x + 1)}  # x > -1/20; monotone increasing function
+ytrue <- function(x) {log(20 * x + 1)}  # x > -1/20; monotone increasing function
 
-given <- list(x = cbind(c(0, 0.1, 0.2, 0.3, 0.4, 0.9, 1)), # inputs where fcn evaluated
-              xstar = cbind(seq(0, 1, length.out = 50)), # prediction set inputs
-              xprime = cbind(c(0.42, 0.47, 0.52, 0.57, 0.62, # derivative set inputs
+# x = inputs where fcn evaluated
+# xstar = prediction set inputs
+# xprime = derivative set inputs
+# true/known model values
+given <- list(x = cbind(c(0, 0.1, 0.2, 0.3, 0.4, 0.9, 1)),
+              xstar = cbind(seq(0, 1, length.out = 50)),
+              xprime = cbind(c(0.42, 0.47, 0.52, 0.57, 0.62,
                                0.67, 0.72, 0.77, 0.82, 0.87)),
-              y = ytrue(c(0, 0.1, 0.2, 0.3, 0.4, 0.9, 1))) # true/known model values
+              y = ytrue(c(0, 0.1, 0.2, 0.3, 0.4, 0.9, 1)) )
 
 TT <- 20  # total time
 tauTT <- 10e-6
-taus <- seq(0, tauTT, length.out = TT+1)  # qqq for now
+taus <- seq(0, tauTT, length.out = TT+1)  # TODO: sequence of taus???
 taus <- taus[2:length(taus)]
 
-N <- 100  # particles
+N <- 3  # particles
 
 ################################################################################
 # 1. initialise (ith row = ith particle) #######################################
 ################################################################################
-particles.l <- matrix(1, ncol = 1, nrow = N)
-particles.sig2 <- matrix(1, ncol = 1, nrow = N)
-particles.ystar <- matrix(NA, ncol = length(xstar), nrow = N)
-particles.yprime <- matrix(NA, ncol = length(xprime), nrow = N)
+# jth colunn = jth time step
+# - store multidimensional things in a list (ystar, yprime)
+particles.l <- matrix(NA, ncol = TT, nrow = N)
+particles.sig2 <- matrix(NA, ncol = TT, nrow = N)
+particles.ystar <- lapply(rep(NA, nrow(given$xstar)),
+                          function(x) matrix(NA, ncol = 1, nrow = N) )
+particles.yprime <- lapply(rep(NA, nrow(given$xprime)),
+                           function(x) matrix(NA, ncol = 1, nrow = N) )
+
+# TODO: initialise t0 i.e. tau0 particles from eta0
+particles.l.t0 <- NULL
+particles.sig2.t0 <- NULL
+particles.ystar.t0 <- NULL
+particles.yprime.t0 <- NULL
 
 ################################################################################
 # 2. weights at time t = 1 #####################################################
@@ -40,10 +54,12 @@ step.ystarynew <- 1  # tune this
 
 # should let step size vary to provide decent acceptance rates (during burn in?)
 
-### Update functions ###########################################################
+### Sampling functions #########################################################
 # return:
 #     current: either accepted proposal, or stay at old for this iteration
 #     accepted: 1 (accepted) or 0 (not accepted)
+
+# ==============================================================================
 update.l <- function(lold,
                      sig2, ystar, yprime,
                      given = given,
@@ -80,6 +96,7 @@ update.l <- function(lold,
 
 }
 
+# ==============================================================================
 update.sig2 <- function(l, sig2old,
                         ystar, yprime,
                         given = given,
@@ -114,7 +131,8 @@ update.sig2 <- function(l, sig2old,
 
 }
 
-update.ystarynew <- function(l, sig2, ystarold, yprimeold,
+# ==============================================================================
+update.ystaryprime <- function(l, sig2, ystarold, yprimeold,
                         given = given,
                         step = step.ystarynew) {
 
@@ -165,29 +183,159 @@ update.ystarynew <- function(l, sig2, ystarold, yprimeold,
   # eigen(tau2.xstarprime)$values  # changed nugget to 10e-6 from -8
 
   ystaryprimenew <- rmvnorm(1, mean = mu.xstarprime, sigma = tau2.xstarprime)
+  ystarnew <- ystaryprimenew[1:nrow(xstar),]
+  yprimenew <- ystaryprimenew[-(1:nrow(xstar)),]
 
-  if
+  logHR <- logposterior(l = l, sig2 = sig2, ystar = ystarnew, yprime = yprimenew,
+                        given = given) -
+    logposterior(l = l, sig2 = sig2, ystar = ystarold, yprimeold,
+                 given = given)
 
-  return(list(sig2current = sig2current,
+  cat("ystaryprime logHR:", logHR)
+
+  if ( !is.nan(logHR) & !is.na(logHR) ) {
+    if ( logHR > log(runif(1)) ) {
+        ystarcurrent <- ystarnew
+        yprimecurrent <- yprimenew
+        accepted <- 1
+    }
+  }
+
+  return(list(ystarcurrent = ystarcurrent,
+              yprimecurrent = yprimecurrent,
               accepted = accepted))
 
 }
 
+# ==============================================================================
+
+combine.particles <- function(out1, out2) {
+
+  return( list(l = c(out1$l, out2$l),
+               sig2 = c(out1$sig2, out2$sig2),
+               ystar = rbind(out1$ystar, out2$ystar),
+               yprime = rbind(out1$yprime, out2$yprime),
+               acceptances = rbind(out1$acceptances, out2$acceptances)) )
+
+}
+
+update.all <- function(t = t, accepted,  # TODO: acceptance rates???
+                       particles.l.t = particles.l.t,
+                       particles.sig2.t = particles.sig2.t,
+                       particles.ystar.t = particles.ystar.t,
+                       particles.yprime.t = particles.yprime.t) { # TODO: fill this in
+
+  foreach(i = 1:N, .combine = "combine.particles") %dopar% {
+
+    lold <- particles.l.t[i]
+    sig2old <- particles.sig2.t[i]
+    # all the "parts" for a single particle are spread accross elements
+    # in a list => need to get the ith entry from each element of the list
+    # to get the full ith particle for ystar and yprime
+    ystarold <- lapply(1:nrow(given$xstar),
+                       function(x) (particles.ystar.t[[x]])[i])
+    ystarold <- unlist(ystarold)  # full ith particle for ystar as a vector
+    yprimeold <- lapply(1:nrow(given$xprime),
+                       function(x) (particles.xprime.t[[x]])[i])
+    yprimeold <- unlist(yprimeold)  # full ith particle for yprime as a vector
+
+    ## current: either accepted proposal, or stay at old for this iteration
+
+    ### UPDATE l ###############################################################
+
+    lupdate <- update.l(lold = lold,
+                        sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
+                        given = given, step = step.l)
+
+    lcurrent <- lupdate$lcurrent
+    accepted.l <- accepted.l + lupdate$accepted
+
+    #cat("\t \t \t -> acceptance rate for l:", accepted.l / i, "\n")
+
+    ### UPDATE sig2 ############################################################
+
+    sig2update <- update.sig2(l = lcurrent, sig2old = sig2old,
+                              ystar = ystarold, yprime = yprimeold,
+                              given = given, step = step.sig2)
+
+    sig2current <- sig2update$sig2current
+    accepted.sig2 <- accepted.sig2 + sig2update$accepted
+
+    #cat("\t \t \t -> acceptance rate for sig2:", accepted.sig2 / i, "\n")
+
+    ### UPDATE ystaryprime #####################################################
+    # did not need to do metropolis for eta_0 (particle initialisation)
+    # because when tau0 = 0 (no constraint), we can sample from the
+    # (ystar, ynew) | l, sig2 directly
+
+    ystaryprimeupdate <- update.ystaryprime(l = lcurrent, sig2old = sig2current,
+                              ystar = ystarold, yprime = yprimeold,
+                              given = given, step = step.ystaryprimenew)
+
+    ystarcurrent <- ystaryprimeupdate$ystarcurrent
+    yprimecurrent <- ystaryprimeupdate$yprimecurrent
+    accepted.ystaryprime <- accepted.ystaryprime + ystaryprimeupdate$accepted
+
+    ### UPDATE chains ##########################################################
+
+    return( list(l = lcurrent,
+                 sig2 = sig2current,
+                 ystar = ystarcurrent,
+                 yprime = yprimecurrent,
+                 acceptances =
+                    cbind(accepted.l = accepted.l,
+                          accepted.sig2 = accepted.sig2,
+                          accepted.ystaryprime = accepted.ystaryprime)) )
+
+  }
+
+}
 
 for (t in 1:TT) {
 
   ## WEIGHT CALCULATION ========================================================
-  yprimet <- apply(particles.yprime, MARGIN = 2, FUN = mean)
+  ## AND t-1 (old) PARTICLES ===================================================
 
   if (t == 1) {
+
+    # TODO: check if you really set particles_1 to particles_0
+    particles.l.old <- particles.l.t <- particles.l.t0
+    particles.sig2.old <- particles.sig2.t <- particles.sig2.t0
+    particles.ystar.old <- particles.ystar.t <- particles.ystar.t0
+    particles.yprime.old <- particles.yprime.t <- particles.yprime.t0
+
+    # ==========================================================================
+    yprimet <- apply(particles.yprime.old, MARGIN = 2, FUN = mean)
+
     wtildes <- prod( pnorm(tau[1] * yprimet) ) /
       prod( pnorm(0 * yprimet) )  # tau0 = 0
 
     W[,1] <- wtildes
 
   } else {
-    wtildes <- prod( pnorm(tau[t] * particles.yprime) ) /
-      prod( pnorm(tau[t-1] * particles.yprime) )
+
+    # t - 1 ITERATION ==========================================================
+    particles.l.old <- particles.l[,t-1]
+    particles.sig2.old <- particles.sig2[,t-1]
+    particles.ystar.old <- lapply(1:nrow(given$xstar),
+                                function(x) (particles.ystar[[x]])[,t-1])
+    particles.yprime.old <- lapply(1:nrow(given$xprime),
+                                function(x) (particles.yprime[[x]])[,t-1])
+
+    # t ITERATION ==============================================================
+    particles.l.t <- particles.l[,t]
+    particles.sig2.t <- particles.sig2[,t]
+    # each list element is for a single input
+    particles.ystar.t <- lapply(1:nrow(given$xstar),
+                                function(x) (particles.ystar[[x]])[,t])
+    particles.yprime.t <- lapply(1:nrow(given$xprime),
+                                function(x) (particles.yprime[[x]])[,t])
+
+    # ==========================================================================
+    yprimet <- apply(particles.yprime.old, MARGIN = 2, FUN = mean)
+
+    wtildes <- prod( pnorm(tau[t] * particles.yprime.old) ) /
+      prod( pnorm(tau[t-1] * particles.yprime.old) )
 
     W[,t] <- W[,t-1] * wtildes
   }
@@ -196,16 +344,18 @@ for (t in 1:TT) {
 
   ## RESAMPLING ================================================================
   ### effective sample size
-  ESSt <- 1 / sum(W[,t])
+  ESSt <- 1 / sum( (W[,t])^2 )
 
   if (ESSt <= N/2) {
 
     resample <- sample(1:N, replace = TRUE, prob = W[,t])
 
-    particles.l <- particles.l[resample]
-    particles.sig2 <- particles.sig2[resample]
-    particles.ystar <- particles.ystar[resample,]
-    particles.yprime <- particles.yprime[resample,]
+    particles.l.t <- particles.l.t[resample]
+    particles.sig2.t <- particles.sig2.t[resample]
+    particles.ystar.t <- lapply(1:nrow(given$xstar),
+                                function(x) (particles.ystar.t[[x]])[resample])
+    particles.yprime.t <- lapply(1:nrow(given$xprime),
+                                function(x) (particles.yprime.t[[x]])[resample])
 
     W[,t] <- 1/N
 
@@ -213,131 +363,37 @@ for (t in 1:TT) {
 
   ## SAMPLING ==================================================================
 
+  ### t + 1 iteration preparation ==============================================
   particles.l.t <- particles.l
   particles.sig2.t <- particles.sig2
   particles.ystar.t <- particles.ystar
   particles.yprime.t <- particles.yprime
 
-  ### Track acceptance rate
-  accepted.l <- accepted.sig2 <- 0
+  ### Track acceptance rate ====================================================
+  accepted.l <- accepted.sig2 <- accepted.ystaryprime <- 0
 
-  particles.t <-
-    foreach(i = 1:N) %dopar% {  # .combine??
+  ### Sample and update chains =================================================
+  particles.new <- update.all()  # TODO: fill this in after update.all done (acceptance rates ???)
+  acceptances <- particles.new$acceptances  # N by 3 matrix of acceptances
 
-      lold <- particles.l[i]
-      sig2old <- particles.sig2[i]
-      ystarold <- particles.ystar[i,]
-      yprimeold <- particles.yprime[i,]
+  particles.l[,t+1] <- particles.new$sig2
+  particles.sig2[,t+1] <- particles.new$sig2current
 
-      ## current: either accepted proposal, or stay at old for this iteration
+  ystar.new <- particles.new$ystar
+  yprime.new <- particles.new$yprime
 
-      ### UPDATE l #############################################################
+  particles.ystar <- lapply(1:length(given$xstar),
+                            function(x) cbind(particles.ystar[[x]], ystar.new[,x]))
 
-      lupdate <- update.l(lold = lold,
-                          sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
-                          given = given, step = step.l)
-
-      lcurrent <- lupdate$lcurrent
-      accepted.l <- accepted.l + lupdate$accepted
-
-      #cat("\t \t \t -> acceptance rate for l:", accepted.l / i, "\n")
-
-      ### UPDATE sig2 ##########################################################
-
-      sig2update <- update.sig2(l = lcurrent, sig2old = sig2old,
-                                ystar = ystarold, yprime = yprimeold,
-                                given = given, step = step.sig2)
-
-      sig2current <- sig2update$sig2current
-      accepted.sig2 <- accepted.sig2 + sig2update$accepted
-
-      #cat("\t \t \t -> acceptance rate for sig2:", accepted.sig2 / i, "\n")
-
-      ### UPDATE ystaryprime ###################################################
-      # did not need to do metropolis for eta_0 (particle initialisation)
-      # because when tau0 = 0 (no constraint), we can sample from the
-      # (ystar, ynew) | l, sig2 directly
-      nugget <- 1e-6  # -7 caused 39/100 warnings
-
-      ## fix this solve; check the nugget
-      Rinv <- solve(covMatrix(X = x, sig2 = sig2current,
-                              covar.fun = r.matern, l = lcurrent) +
-                      diag(nugget, nrow(x)))
-
-      S11 <- covMatrix(X = xstar, sig2 = sig2current, covar.fun = r.matern, l = lcurrent)
-      S22 <- covMatrix(X = xprime, sig2 = sig2current, covar.fun = r.matern2, l = lcurrent)
-      S21 <- covMatrix(X = xprime, X2 = xstar,  # CAREFUL SEE PAPER FOR ARG. ORDER
-                       sig2 = sig2current, covar.fun = r.matern1, l = lcurrent)
-      R.xstarxprime <- rbind(cbind(S11, t(S21)),
-                             cbind(S21, S22)) +
-        diag(nugget, nrow(xstar) + nrow(xprime))
-
-      # CAREFUL SEE PAPER FOR ARG. ORDER
-      r.xstarprime.x <- rbind(covMatrix(X = xstar, X2 = x, sig2 = sig2current,
-                                        covar.fun = r.matern, l = lcurrent),
-                              covMatrix(X = xprime, X2 = x, sig2 = sig2current,
-                                        covar.fun = r.matern1, l = lcurrent))
-      mu.xstarprime <- r.xstarprime.x %*% Rinv %*% y
-
-      # because covMatrix has sig2 in it, have an extra one multiplying in???
-      tau2.xstarprime <- R.xstarxprime -
-        r.xstarprime.x %*% Rinv %*% t(r.xstarprime.x) #/ sig2current #???
-
-      # MAKE SURE tau2.xstarprime IS SYMMETRIC
-      tau2.xstarprime <- ( tau2.xstarprime + t(tau2.xstarprime) ) / 2
-
-      # MAKE SURE tau2.xstarprime IS POSTIVE SEMI-DEFINITE
-      # eigen(tau2.xstarprime)$values  # changed nugget to 10e-6 from -8
-
-
-      ystaryprimenew <- rmvnorm(1, mean = mu.xstarprime, sigma = tau2.xstarprime)
-      ystarnew <- ystaryprimenew[1:length(xstar)]
-      yprimenew <- ystaryprimenew[(length(xstar)+1):length(ystaryprimenew)]
-
-      particles.ystar.t[i,] <- ystarnew
-      particles.yprime.t[i,] <- yprimenew
-
-    }
+  particles.yprime <- lapply(1:length(given$xprime),
+                             function(x) cbind(particles.yprime[[x]], yprime.new[,x]))
+  particles.ystarcurrent  # TODO: adding new particles to chain
+                              #  particles.sig2 <- matrix(NA, ncol = TT, nrow = N)
+                              #  particles.ystar <- lapply(rep(NA, nrow(given$xstar)),
+                              #                            function(x) matrix(NA, ncol = TT, nrow = N) )
+                              #  particles.yprime <- lapply(rep(NA, nrow(given$xprime)),
+                              #                             function(x) matrix(NA, ncol = TT, nrow = N) )
 
 
 
-
-}
-
-
-
-scmc = function(tausteps = seq(from = 0, to = 10, length = 100),
-                priorsamples,
-                constraint = 0,
-                input = suse,
-                par = par,
-                type = type) {
-  #odefn(suse,par,type)
-  #timestep=10;prior_fun=odefn;post_fun=m_state_svec;input=suse;par=par;type=type
-
-  theta     = priorsamples
-  w         = priorsamples[, 1] * 0 + 1#The value of w   numeric(timestep)
-  W         = w / dim(priorsamples)[1] #The value of W
-  step = 0
-  for (tau in tausteps[-1]) {
-    step = step + 1
-    w = pnorm(tau * theta) / pnorm(taustep[step + 1] * theta)#updata wj
-    W = w * W#update Wj
-    # W[,(ts+1)] = (W[,(ts+1)]-mean(W[,(ts+1)]))/sd(W[,(ts+1)]) #Normalize Wj
-    ESS = sum(W ^ 2)
-    if (ESS < (length(W) / 2)) {
-      theta = sample(ori.post, prob = W)#resample
-      W     = 1 / dim(priorsamples)[1] #The value of W
-    } else{
-      theta  = sample(ori.post[, 1], prob = W[, (ts + 1)] / sum(W[, (ts + 1)]))#need to change mh step
-    }
-  }#end of time step
-  after.post = matrix(
-    theta[, timestep + 1],
-    byrow = F,
-    ncol = ncol(post_fun),
-    nrow = nrow(post_fun)
-  )
-  return(after.post)
-  b
 }
