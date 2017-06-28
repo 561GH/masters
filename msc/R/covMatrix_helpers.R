97388efaa36a97ed9c30e2d01019b386851ae5ef
GGH <ggh@d207-023-185-117.wireless.sfu.ca>
2017-06-28 15:11:09 -0700

    vastly simplified all covariance helper functions (why did I make it so complicated before??)

--- msc/R/covMatrix_helpers.R
+++ msc/R/covMatrix_helpers.R
@@ -36,36 +36,11 @@ r.matern <- function(xi, xj,
   # if xi == xj ################################################################
   if (isTRUE(all.equal(xi, xj))) {return(1)}  # multiply by sig2 in covMatrix()
 
-  # g FUNCTION START ###########################################################
-  # g(.): from Matern classs of covariance functions
-  ## modified bessel function of the second kind
-  ## https://stat.ethz.ch/pipermail/r-help/2002-December/027850.html
-  g <- function(input) {
-    # input: 3-dimensional vector c(xik, xjk, ltransk)
-    ## xik, xjk: kth entry of vectors xi, xj respectively
-    ## ltransk: kth entry of ltrans as defined for r(., .); always ltrans, not l
-
-    xik <- input[1]
-    xjk <- input[2]
-    ltransk <- input[3]
-
-    ## for general lambda
-    2^(1 - lambda) / gamma(lambda) *
-      ( ltransk * abs(xik - xjk) ) ^ lambda *
-      besselK( ltransk * abs(xik - xjk), nu = lambda)
-
-    ## for lambda = 5/2; used to check if the above is correct
-    # (1 + ltransk * abs(xik - xjk) + 1/3 * (ltransk * abs(xik - xjk))^2) *
-    #   exp(-ltransk * abs(xik - xjk))
-
-  }
-  # g FUNCTION END #############################################################
-
   # xi != xj ###################################################################
-  xixjltrans <- cbind(xi, xj, ltrans)
-  covxixj <- prod( apply(xixjltrans, MARGIN = 1, FUN = g) )
+  g.xixj <- (1 + ltrans * abs(xi - xj) + 1/3 * (ltrans * abs(xi - xj))^2) *
+    exp(-ltrans * abs(xi - xj))
 
-  return(covxixj)
+  return( prod( g.xixj ) )
 
 }
 
@@ -107,33 +82,14 @@ r.matern1 <- function(xi, xj,
   # if xi == xj ################################################################
   if (isTRUE(all.equal(xi, xj))) {return(1)}  # multiply by sig2 in covMatrix()
 
-  # g FUNCTION START ###########################################################
-  # g(.): from Matern classs of covariance functions
-  ## modified bessel function of the second kind
-  ## https://stat.ethz.ch/pipermail/r-help/2002-December/027850.html
-  g <- function(input) {
-    # input: 3-dimensional vector c(xik, xjk, ltransk)
-    ## xik, xjk: kth entry of vectors xi, xj respectively
-    ## ltransk: kth entry of ltrans as defined for r(., .); always ltrans, not l
-
-    xik <- input[1]
-    xjk <- input[2]
-    ltransk <- input[3]
-
-    ## for lambda = 5/2
-    1/3 * ltransk^2 * abs(xik - xjk) *
-      (1 + ltransk * abs(xik - xjk)) *
-      exp( -ltransk * abs(xik - xjk) ) *
-      sign(xik - xjk)
-
-  }
-  # g FUNCTION END #############################################################
-
   # xi != xj ###################################################################
-  xixjltrans <- cbind(xi, xj, ltrans)
-  covxixj <- prod( apply(xixjltrans, MARGIN = 1, FUN = g) )
+  ## for lambda = 5/2
+  g.xixj <- 1/3 * ltrans^2 * abs(xi - xj) *
+    (1 + ltrans * abs(xi - xj)) *
+    exp( -ltrans * abs(xi - xj) ) *
+    sign(xi - xj)
 
-  return(covxixj)
+  return( prod(g.xixj) )
 
 }
 
@@ -173,31 +129,12 @@ r.matern2 <- function(xi, xj,
   # if xi == xj ################################################################
   if (isTRUE(all.equal(xi, xj))) {return(1)}  # multiply by sig2 in covMatrix()
 
-  # g FUNCTION START ###########################################################
-  # g(.): from Matern classs of covariance functions
-  ## modified bessel function of the second kind
-  ## https://stat.ethz.ch/pipermail/r-help/2002-December/027850.html
-  g <- function(input) {
-    # input: 3-dimensional vector c(xik, xjk, ltransk)
-    ## xik, xjk: kth entry of vectors xi, xj respectively
-    ## ltransk: kth entry of ltrans as defined for r(., .); always ltrans, not l
-
-    xik <- input[1]
-    xjk <- input[2]
-    ltransk <- input[3]
-
-    ## for lambda = 5/2
-    1/3 * ltransk^2 * (1 + ltransk * abs(xik - xjk) -
-                        0.5 * (ltransk * abs(xik - xjk))^2 ) *
-      exp( -ltrans * abs(xik - xjk) ) * sign(xik - xjk)
-
-  }
-  # g FUNCTION END #############################################################
-
   # xi != xj ###################################################################
-  xixjltrans <- cbind(xi, xj, ltrans)
-  covxixj <- prod( apply(xixjltrans, MARGIN = 1, FUN = g) )
+  ## for lambda = 5/2
+  g.xixj <- 1/3 * ltrans^2 * (1 + ltrans * abs(xi - xj) -
+                                0.5 * (ltrans * abs(xi - xj))^2 ) *
+    exp( -ltrans * abs(xi - xj) ) * sign(xi - xj)
 
-  return(covxixj)
+  return( prod(g.xixj) )
 
 }
