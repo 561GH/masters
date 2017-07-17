74182478d620b7604cc1c23dc67b7a60947e377b
GGH <ggh@d207-023-189-085.wireless.sfu.ca>
2017-07-17 11:23:20 -0700

    use new function for kriging mean and covariance

--- msc/R/logposterior.R
+++ msc/R/logposterior.R
@@ -19,8 +19,14 @@ logposterior <- function(l,
   x <- cbind(given$x)
 
   ## means and covariances
-  m <- rep(0, length(ystar) + length(yprime))
   my <- rep(0, length(y)) #rep(mean(y), length(y))
+
+  nugget <- 1e-6
+  R <- covMatrix(X1 = x, X2 = x, sig2 = sig2, l = l,
+                 covar.fun = "matern") +
+    diag(nugget, nrow(x))
+  Linv <- solve( t(chol(R)) )  # recall need transpose to match std chol def
+  Rinv <- t(Linv) %*% Linv
 
   S11 <- covMatrix(X1 = xstar, X2 = xstar,
                    sig2 = sig2, l = l, covar.fun = "matern")
@@ -28,15 +34,33 @@ logposterior <- function(l,
                    sig2 = sig2, l = l, covar.fun = "matern2", d1 = 1, d2 = 1)
   S21 <- covMatrix(X1 = xprime, X2 = xstar,  # CAREFUL SEE PAPER FOR ARG. ORDER
                    sig2 = sig2, l = l, covar.fun = "matern1", d1 = 1)
-  S <- rbind(cbind(S11, t(S21)),
-             cbind(S21, S22))
 
-  Syy <- covMatrix(X1 = x, X2 = x,
-                   sig2 = sig2, l = l, covar.fun = "matern")
+  R.xstarxprime <- rbind(cbind(S11, t(S21)),
+                         cbind(S21, S22)) +
+    diag(nugget, nrow(xstar) + nrow(xprime))
+
+  # CAREFUL SEE PAPER FOR ARG. ORDER
+  r.xstarprime.x <- rbind(covMatrix(X1 = xstar, X2 = x,
+                                    sig2 = sig2, l = l,
+                                    covar.fun = "matern"),
+                          covMatrix(X1 = xprime, X2 = x,
+                                    sig2 = sig2, l = l,
+                                    covar.fun = "matern1", d1 = 1))
+  mu.xstarprime <- r.xstarprime.x %*% Rinv %*% y
+
+  # because covMatrix has sig2 in it, have an extra one multiplying in???
+  # (I think the paper is wrong... following Rasmussen (2.19) instead)
+  tau2.xstarprime <- R.xstarxprime -
+    r.xstarprime.x %*% Rinv %*% t(r.xstarprime.x)
+
+  # MAKE SURE tau2.xstarprime IS SYMMETRIC
+  tau2.xstarprime <- ( tau2.xstarprime + t(tau2.xstarprime) ) / 2
 
   ## use log scale because numbers are so tiny
-  log.d.ystaryprime <- dmvnorm(c(ystar, yprime), mean = m, sigma = S, log = TRUE)
-  log.d.y <- dmvnorm(as.numeric(y), mean = my, sigma = Syy, log = TRUE)
+  log.d.ystaryprime <- dmvnorm(c(ystar, yprime),
+                               mean = mu.xstarprime, sigma = tau2.xstarprime,
+                               log = TRUE)
+  log.d.y <- dmvnorm(as.numeric(y), mean = my, sigma = R, log = TRUE)
 
   # cat("log.d.ystaryprime:", log.d.ystaryprime, "\n")
   # cat("log.d.y:", log.d.y, "\n")
