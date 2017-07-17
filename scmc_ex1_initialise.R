82cfaad6c3273dff1a25754fc94ad957556c4a06
GGH <ggh@d207-023-189-085.wireless.sfu.ca>
2017-07-17 11:25:40 -0700

    super oops: many mistakes in hastings ratio calculations - this should have fixed all of them - also awkward… didn’t actually need step size for sig2 proposal

--- scmc_ex1_initialise.R
+++ scmc_ex1_initialise.R
@@ -21,8 +21,7 @@ given <- list(x = cbind(c(0, 0.1, 0.2, 0.3, 0.4, 0.9, 1)),
 eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
                  given,  # data, locations, obs, etc.
                  N = 100, # iterations
-                 v1 = 0.01,  # step size for l proposal
-                 v2 = 3) {# step size for sig2 proposal
+                 v1 = 0.01) {  # step size for l proposal
 
   x <- given$x
   xstar <- given$xstar
@@ -48,7 +47,7 @@ eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
       yprimeold <- eta.init$yprime
     } else {
       lold <- chain.l[i-1]
-      sig2old <- chain.l[i-1]
+      sig2old <- chain.sig2[i-1]
       ystarold <- chain.ystar[i-1,]
       yprimeold <- chain.yprime[i-1,]
     }
@@ -60,38 +59,47 @@ eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
 
     ## UPDATE l ##################################################################
     lnew <- rnorm(1, mean = lold, sd = v1)
+    while (lnew < 0.05) {  # ensures that l isn't too stupidly small
+      lnew <- rnorm(1, mean = lold, sd = v1)
+    }
 
-    if (lnew > 0) {
+    num <- logposterior(l = lnew,
+                        sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
+                        given = given) +
+      log( pnorm(lold, mean = lnew, sd = v1) )#/ pnorm(0.05, mean = lnew, sd = v1) )
+    den <- logposterior(l = lold,
+                        sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
+                        given = given) +
+      log( pnorm(lnew, mean = lold, sd = v1) )#/ pnorm(0.05, mean = lold, sd = v1) )
+    logHR <- num - den
 
-      logHR <- logposterior(l = lnew,
-                            sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
-                            given = given) -
-        logposterior(l = lold,
-                     sig2 = sig2old, ystar = ystarold, yprime = yprimeold,
-                     given = given)
+    #cat("lnew logHR:", logHR)
 
-      #cat("lnew logHR:", logHR)
-
-      if ( !is.nan(logHR) & !is.na(logHR) ) {  # when have -Inf - Inf get NaN
-        if ( logHR > log(runif(1)) )  {
-          chain.l[i] <- lnew
-          accepted.l[i] <- 1
-        }
+    if ( !is.nan(logHR) & !is.na(logHR) ) {  # when have -Inf - Inf get NaN
+      if ( logHR > log(runif(1)) )  {
+        chain.l[i] <- lnew
+        accepted.l[i] <- 1
       }
-
     }
 
     lcurrent <- chain.l[i]
     cat("\t \t \t -> acceptance rate for l:", sum(accepted.l) / i, "\n")
 
     ## UPDATE sig2 ###############################################################
-    sig2new <- rchisq(1, df = sig2old) #rgamma(1, shape = sig2old/2, scale = 2)
-    logHR <- logposterior(l = lcurrent, sig2 = sig2new,
-                          ystar = ystarold, yprime = yprimeold,
-                          given = given) -
-      logposterior(l = lcurrent, sig2 = sig2old,
-                   ystar = ystarold, yprime = yprimeold,
-                   given = given)
+    sig2new <- rchisq(1, df = sig2old)
+    while (sig2new <= 1.7) {  # arbitrary minimum following shirin
+      sig2new <- rchisq(1, df = sig2old)
+    }
+
+    num <- logposterior(l = lcurrent, sig2 = sig2new,
+                        ystar = ystarold, yprime = yprimeold,
+                        given = given) +
+      log( pchisq(sig2new, df = sig2old) ) #/ pchisq(1.7, df = sig2old) )
+    den <- logposterior(l = lcurrent, sig2 = sig2old,
+                        ystar = ystarold, yprime = yprimeold,
+                        given = given) +
+      log( pchisq(sig2old, df = sig2new) ) #/ pchisq(1.7, df = sig2new) )
+    logHR <- num - den
 
     #cat("sig2new logHR:", logHR)
 
@@ -108,41 +116,10 @@ eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
     cat("\t \t \t -> acceptance rate for sig2:", sum(accepted.sig2) / i, "\n")
 
     ## UPDATE ystaryprime ########################################################
-    nugget <- 1e-6
-    R <- covMatrix(X1 = x, X2 = x, sig2 = sig2current, l = lcurrent,
-                   covar.fun = "matern") +
-      diag(nugget, nrow(x))
-    Linv <- solve( t(chol(R)) )  # recall need transpose to match std chol def
-    Rinv <- t(Linv) %*% Linv
-    # Rinv <- solve(R)
-
-    S11 <- covMatrix(X1 = xstar, X2 = xstar, sig2 = sig2current, l = lcurrent,
-                     covar.fun = "matern")
-    S22 <- covMatrix(X1 = xprime, X2 = xprime, sig2 = sig2current, l = lcurrent,
-                     covar.fun = "matern2", d1 = 1, d2 = 1)
-    S21 <- covMatrix(X1 = xprime, X2 = xstar,  # CAREFUL SEE PAPER FOR ARG. ORDER
-                     sig2 = sig2current, l = lcurrent,
-                     covar.fun = "matern1", d1 = 1)
-    R.xstarxprime <- rbind(cbind(S11, t(S21)),
-                           cbind(S21, S22)) +
-      diag(nugget, nrow(xstar) + nrow(xprime))
-
-    # CAREFUL SEE PAPER FOR ARG. ORDER
-    r.xstarprime.x <- rbind(covMatrix(X1 = xstar, X2 = x,
-                                      sig2 = sig2current, l = lcurrent,
-                                      covar.fun = "matern"),
-                            covMatrix(X1 = xprime, X2 = x,
-                                      sig2 = sig2current, l = lcurrent,
-                                      covar.fun = "matern1", d1 = 1))
-    mu.xstarprime <- r.xstarprime.x %*% Rinv %*% y
-
-    # because covMatrix has sig2 in it, have an extra one multiplying in???
-    # (I think the paper is wrong... following Rasmussen (2.19) instead)
-    tau2.xstarprime <- R.xstarxprime -
-      r.xstarprime.x %*% Rinv %*% t(r.xstarprime.x)
-
-    # MAKE SURE tau2.xstarprime IS SYMMETRIC
-    tau2.xstarprime <- ( tau2.xstarprime + t(tau2.xstarprime) ) / 2
+    pred.parameters <- predictiveMeanCov(given = given,
+                                         l = lcurrent, sig2 = sig2current)
+    mu.xstarprime <- pred.parameters$mu.xstarprime
+    tau2.xstarprime <- pred.parameters$cov.xstarprime
 
     # MAKE SURE tau2.xstarprime IS POSTIVE SEMI-DEFINITE
     # eigen(tau2.xstarprime)$values
@@ -173,15 +150,17 @@ eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
 
 ################################################################################
 # # test
-# burn <- 2000
-# N <- 1000
-# init <- eta0(eta.init = list(l = 0.5, sig2 = 0.1,
+# burn <- 1
+# N <- 100
+# init <- eta0(eta.init = list(l = 0.5, sig2 = 5,
 #                              ystar = ytrue(given$xstar) - mean(ytrue(given$x)),
 #                              yprime = 20 / (20 * given$xprime) ),
 #              given = given,  # data, locations, obs, etc.)
 #              N = burn + N, # particles
-#              v1 = 0.05, # step size for l proposal
-#              v2 = 0.01) # step size for sig2 proposal
+#              v1 = 0.07) # step size for l proposal
+#
+# hist(init$chain.l[-(1:burn)])
+# hist(init$chain.sig2[-(1:burn)])
 #
 #
 # particles.yprime <- init$chain.yprime[-(1:burn),]
@@ -189,12 +168,13 @@ eta0 <- function(eta.init,  # initial values for l, sig2, ystar, yprime is list
 # for (n in (N-50):N) {
 #   points(x = given$xprime, y = particles.yprime[n,], type = "l", col = n)
 # }
+# points(x = given$xprime, y = apply(particles.yprime, 2, mean), pch = 16, col = "red")
 # tail(particles.yprime)
 #
 # particles.ystar <- init$chain.ystar[-(1:burn),]
 # tail(particles.ystar)
 # plot(x = given$x, y = given$y, pch = 16, cex = 2)
-# for (n in (N-100):N) {
+# for (n in (N-50):N) {
 #   points(x = given$xstar, y = particles.ystar[n,], type = "l", col = "red")
 # }
 #
