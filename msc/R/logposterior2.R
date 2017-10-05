d1298ea8ea57e241d5a0b0a546182c2ec9f51bfb
GGH <ggh@d207-023-191-068.wireless.sfu.ca>
2017-10-05 13:04:16 -0700

    update after predictiveMeanCov2() function update + some general tidying

--- msc/R/logposterior2.R
+++ msc/R/logposterior2.R
@@ -19,31 +19,23 @@ logposterior2 <- function(l,
 
   ## means and covariances
   nugget <- 0
-  kriging <- predictiveMeanCov2(given = given, l = l, sig2 = sig2,
-                                nugget1 = 0, nugget2 = 0)
+  kriging <- predictiveMeanCov2(given = given, l = l, sig2 = sig2)
 
   mu.xstarprime <- kriging$mu.xstarprime
   tau2.xstarprime <- kriging$cov.xstarprime +
     diag(rep(2, nrow(xstar) + nrow(xprime)))
-  R <- kriging$R.xx
-
-  # A <- chol(tau2.xstarprime)
-  # min(apply(tau2.xstarprime, MARGIN = 1, sum) - diag(tau2.xstarprime))
-  # eigen(tau2.xstarprime)$values
 
   ## use log scale because numbers are so tiny
-
   log.d.y <- dmvn(as.numeric(y),
-                  m = rep(0, length(y)), S = R, log = TRUE)
+                  m = rep(0, length(y)), S = kriging$R.xx,
+                  log = TRUE)
 
   log.d.ystaryprime <- dmvn(c(ystar, yprime),
                             m = mu.xstarprime, S = tau2.xstarprime,
                             log = TRUE)
 
-  # cat("log.d.ystaryprime:", log.d.ystaryprime, "\n")
-  # cat("log.d.y:", log.d.y, "\n")
-
-  return( sum( log( dchisq(sqrt(5) / l, df = 1) ) ) + log( dchisq(sig2, df = 5) ) +
+  return( sum( log( dchisq(sqrt(5) / l, df = 1) ) ) +
+            log( dchisq(sig2, df = 5) ) +
             log.d.ystaryprime + log.d.y )
 
 }
